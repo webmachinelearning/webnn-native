b0efa9e4e5fbc42bfe400501c730ce8fd92fc1f6
Wanming Lin <wanming.lin@intel.com>
2022-05-11 17:23:11 +0800

    [Sample] Support WebNN and Polyfill switch in one page

---
+++ node/examples/electron/webnn-samples/node_setup.js
@@ -0,0 +1,12 @@
+global.webnn = require('../../lib/webnn');
+// navigator is undefined in node.js, but defined in electron.js.
+if (global.navigator === undefined) {
+  global.navigator = {};
+}
+global.navigator.ml = webnn.ml;
+global.MLContext = webnn.MLContext;
+global.MLGraphBuilder = webnn.MLGraphBuilder;
+global.MLGraph = webnn.MLGraph;
+global.MLOperand = webnn.MLOperand;
+global.chai = require('chai');
+global.fs = require('fs');
